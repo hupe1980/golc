7382ce62c1cff3371daf54331292ebc4a58775a4
hupe1980 <frankhuebner1980@gmail.com>
2023-07-04 23:26:38 +0200

    Add docstrings

--- prompt/chat.go
+++ prompt/chat.go
@@ -6,39 +6,47 @@ import (
 	"github.com/hupe1980/golc/schema"
 )
 
+// ChatPromptValue represents a chat prompt value containing chat messages.
 type ChatPromptValue struct {
 	messages schema.ChatMessages
 }
 
+// NewChatPromptValue creates a new ChatPromptValue with the given chat messages.
 func NewChatPromptValue(messages schema.ChatMessages) *ChatPromptValue {
 	return &ChatPromptValue{
 		messages: messages,
 	}
 }
 
+// String returns a string representation of the ChatPromptValue.
 func (v ChatPromptValue) String() (string, error) {
 	return v.messages.Format()
 }
 
+// Messages returns the chat messages contained in the ChatPromptValue.
 func (v ChatPromptValue) Messages() schema.ChatMessages {
 	return v.messages
 }
 
+// ChatTemplate represents a chat  template.
 type ChatTemplate interface {
 	FormatPrompt(values map[string]any) (*ChatPromptValue, error)
 	Format(values map[string]any) (schema.ChatMessages, error)
 }
 
+// chatTemplateWrapper wraps multiple ChatTemplates and provides combined formatting.
 type chatTemplateWrapper struct {
 	chatTemplates []ChatTemplate
 }
 
+// NewChatTemplateWrapper creates a new ChatTemplate that wraps multiple ChatTemplates.
 func NewChatTemplateWrapper(chatTemplates ...ChatTemplate) ChatTemplate {
 	return &chatTemplateWrapper{
 		chatTemplates: chatTemplates,
 	}
 }
 
+// FormatPrompt formats the prompt using the provided values and returns a ChatPromptValue.
 func (ct *chatTemplateWrapper) FormatPrompt(values map[string]any) (*ChatPromptValue, error) {
 	messages, err := ct.Format(values)
 	if err != nil {
@@ -48,6 +56,7 @@ func (ct *chatTemplateWrapper) FormatPrompt(values map[string]any) (*ChatPromptV
 	return NewChatPromptValue(messages), nil
 }
 
+// Format formats the messages using the provided values and returns the resulting ChatMessages.
 func (ct *chatTemplateWrapper) Format(values map[string]any) (schema.ChatMessages, error) {
 	fullMeessages := schema.ChatMessages{}
 
@@ -63,16 +72,19 @@ func (ct *chatTemplateWrapper) Format(values map[string]any) (schema.ChatMessage
 	return fullMeessages, nil
 }
 
+// chatTemplate represents a chat message template.
 type chatTemplate struct {
 	messageTemplates []MessageTemplate
 }
 
+// NewChatTemplate creates a new ChatTemplate with the given message templates.
 func NewChatTemplate(messageTemplates []MessageTemplate) ChatTemplate {
 	return &chatTemplate{
 		messageTemplates: messageTemplates,
 	}
 }
 
+// FormatPrompt formats the prompt using the provided values and returns a ChatPromptValue.
 func (ct *chatTemplate) FormatPrompt(values map[string]any) (*ChatPromptValue, error) {
 	messages, err := ct.Format(values)
 	if err != nil {
@@ -82,6 +94,7 @@ func (ct *chatTemplate) FormatPrompt(values map[string]any) (*ChatPromptValue, e
 	return NewChatPromptValue(messages), nil
 }
 
+// Format formats the messages using the provided values and returns the resulting ChatMessages.
 func (ct *chatTemplate) Format(values map[string]any) (schema.ChatMessages, error) {
 	messages := make(schema.ChatMessages, len(ct.messageTemplates))
 
@@ -97,16 +110,19 @@ func (ct *chatTemplate) Format(values map[string]any) (schema.ChatMessages, erro
 	return messages, nil
 }
 
+// messagesPlaceholder represents a placeholder for chat messages.
 type messagesPlaceholder struct {
 	inputKey string
 }
 
+// NewMessagesPlaceholder creates a new ChatTemplate placeholder for chat messages.
 func NewMessagesPlaceholder(inputKey string) ChatTemplate {
 	return &messagesPlaceholder{
 		inputKey: inputKey,
 	}
 }
 
+// FormatPrompt formats the prompt using the provided values and returns a ChatPromptValue.
 func (ct *messagesPlaceholder) FormatPrompt(values map[string]any) (*ChatPromptValue, error) {
 	messages, err := ct.Format(values)
 	if err != nil {
@@ -116,6 +132,7 @@ func (ct *messagesPlaceholder) FormatPrompt(values map[string]any) (*ChatPromptV
 	return NewChatPromptValue(messages), nil
 }
 
+// Format formats the messages using the provided values and returns the resulting ChatMessages.
 func (ct *messagesPlaceholder) Format(values map[string]any) (schema.ChatMessages, error) {
 	messages, ok := values[ct.inputKey].(schema.ChatMessages)
 	if !ok {
@@ -125,20 +142,24 @@ func (ct *messagesPlaceholder) Format(values map[string]any) (schema.ChatMessage
 	return messages, nil
 }
 
+// MessageTemplate represents a chat message template.
 type MessageTemplate interface {
 	Format(values map[string]any) (schema.ChatMessage, error)
 }
 
+// SystemMessageTemplate represents a system message template.
 type SystemMessageTemplate struct {
 	prompt *Template
 }
 
+// NewSystemMessageTemplate creates a new SystemMessageTemplate with the given template.
 func NewSystemMessageTemplate(template string) *SystemMessageTemplate {
 	return &SystemMessageTemplate{
 		prompt: NewTemplate(template),
 	}
 }
 
+// Format formats the message using the provided values and returns a SystemChatMessage.
 func (pt *SystemMessageTemplate) Format(values map[string]any) (schema.ChatMessage, error) {
 	text, err := pt.prompt.Format(values)
 	if err != nil {
@@ -148,16 +169,19 @@ func (pt *SystemMessageTemplate) Format(values map[string]any) (schema.ChatMessa
 	return schema.NewSystemChatMessage(text), nil
 }
 
+// AIMessageTemplate represents an AI message template.
 type AIMessageTemplate struct {
 	prompt *Template
 }
 
+// NewAIMessageTemplate creates a new AIMessageTemplate with the given template.
 func NewAIMessageTemplate(template string) *AIMessageTemplate {
 	return &AIMessageTemplate{
 		prompt: NewTemplate(template),
 	}
 }
 
+// Format formats the message using the provided values and returns an AIChatMessage.
 func (pt *AIMessageTemplate) Format(values map[string]any) (schema.ChatMessage, error) {
 	text, err := pt.prompt.Format(values)
 	if err != nil {
@@ -167,16 +191,19 @@ func (pt *AIMessageTemplate) Format(values map[string]any) (schema.ChatMessage, 
 	return schema.NewAIChatMessage(text), nil
 }
 
+// HumanMessageTemplate represents a human message template.
 type HumanMessageTemplate struct {
 	prompt *Template
 }
 
+// NewHumanMessageTemplate creates a new HumanMessageTemplate with the given template.
 func NewHumanMessageTemplate(template string) *HumanMessageTemplate {
 	return &HumanMessageTemplate{
 		prompt: NewTemplate(template),
 	}
 }
 
+// Format formats the message using the provided values and returns a HumanChatMessage.
 func (pt *HumanMessageTemplate) Format(values map[string]any) (schema.ChatMessage, error) {
 	text, err := pt.prompt.Format(values)
 	if err != nil {
--- prompt/chat_test.go
+++ prompt/chat_test.go
@@ -1,1 +1,84 @@
 package prompt
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/hupe1980/golc/schema"
+)
+
+func TestChatTemplateWrapper(t *testing.T) {
+	// Create some sample chat templates
+	chatTemplate1 := NewSystemMessageTemplate("Welcome, {{.Name}}!")
+	chatTemplate2 := NewAIMessageTemplate("Hello, I'm an AI.")
+	chatTemplate3 := NewHumanMessageTemplate("How can I help you, {{.Name}}?")
+
+	chatTemplates1 := NewChatTemplate([]MessageTemplate{chatTemplate1})
+	chatTemplates2 := NewChatTemplate([]MessageTemplate{chatTemplate2, chatTemplate3})
+
+	// Create the chat template wrapper
+	chatTemplateWrapper := NewChatTemplateWrapper(chatTemplates1, chatTemplates2)
+
+	// Define the input values
+	values := map[string]interface{}{
+		"Name": "John",
+	}
+
+	// Run the test cases
+	t.Run("FormatPrompt", func(t *testing.T) {
+		expectedMessages := schema.ChatMessages{
+			schema.NewSystemChatMessage("Welcome, John!"),
+			schema.NewAIChatMessage("Hello, I'm an AI."),
+			schema.NewHumanChatMessage("How can I help you, John?"),
+		}
+
+		// Call the FormatPrompt method
+		promptValue, err := chatTemplateWrapper.FormatPrompt(values)
+
+		// Check the result
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessages, promptValue.Messages())
+	})
+
+	t.Run("Format", func(t *testing.T) {
+		expectedMessages := schema.ChatMessages{
+			schema.NewSystemChatMessage("Welcome, John!"),
+			schema.NewAIChatMessage("Hello, I'm an AI."),
+			schema.NewHumanChatMessage("How can I help you, John?"),
+		}
+
+		// Call the Format method
+		messages, err := chatTemplateWrapper.Format(values)
+
+		// Check the result
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessages, messages)
+	})
+}
+
+func TestMessagesPlaceholder(t *testing.T) {
+	// Create a chat template placeholder
+	placeholder := NewMessagesPlaceholder("Messages")
+
+	// Define the input values
+	values := map[string]interface{}{
+		"Messages": schema.ChatMessages{
+			schema.NewSystemChatMessage("Hello"),
+			schema.NewHumanChatMessage("How are you?"),
+			schema.NewAIChatMessage("I'm fine, thank you!"),
+		},
+	}
+
+	// Run the test case
+	t.Run("Format", func(t *testing.T) {
+		expectedMessages, _ := values["Messages"].(schema.ChatMessages)
+
+		// Call the Format method
+		messages, err := placeholder.Format(values)
+
+		// Check the result
+		assert.NoError(t, err)
+		assert.Equal(t, expectedMessages, messages)
+	})
+}
