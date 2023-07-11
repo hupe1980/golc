9b905c35fb4f7ccf7d52a1dba2ae7f7d5ef6eb76
hupe1980 <frankhuebner1980@gmail.com>
2023-07-11 20:40:04 +0200

    Add palm chatmodel support

---
+++ model/chatmodel/palm.go
@@ -0,0 +1,143 @@
+package chatmodel
+
+import (
+	"context"
+
+	generativelanguagepb "cloud.google.com/go/ai/generativelanguage/apiv1beta2/generativelanguagepb"
+	"github.com/googleapis/gax-go/v2"
+	"github.com/hupe1980/golc"
+	"github.com/hupe1980/golc/callback"
+	"github.com/hupe1980/golc/schema"
+	"github.com/hupe1980/golc/tokenizer"
+	"github.com/hupe1980/golc/util"
+)
+
+// PalmClient is the interface for the PALM client.
+type PalmClient interface {
+	GenerateMessage(ctx context.Context, req *generativelanguagepb.GenerateMessageRequest, opts ...gax.CallOption) (*generativelanguagepb.GenerateMessageResponse, error)
+}
+
+// PalmOptions is the options struct for the PALM chat model.
+type PalmOptions struct {
+	*schema.CallbackOptions `map:"-"`
+	schema.Tokenizer        `map:"-"`
+
+	// ModelName is the name of the Palm chat model to use.
+	ModelName string `map:"model_name,omitempty"`
+
+	// Temperature is the sampling temperature to use during text generation.
+	Temperatur float32 `map:"temperatur,omitempty"`
+
+	// TopP is the total probability mass of tokens to consider at each step.
+	TopP float32 `map:"top_p,omitempty"`
+
+	// TopK determines how the model selects tokens for output.
+	TopK int32 `map:"top_k"`
+
+	// CandidateCount specifies the number of candidates to generate during text completion.
+	CandidateCount int32 `map:"candidate_count"`
+}
+
+// Palm is a struct representing the PALM language model.
+type Palm struct {
+	client PalmClient
+	opts   PalmOptions
+}
+
+// NewPalm creates a new instance of the PALM language model.
+func NewPalm(client PalmClient, optFns ...func(o *PalmOptions)) (*Palm, error) {
+	opts := PalmOptions{
+		CallbackOptions: &schema.CallbackOptions{
+			Verbose: golc.Verbose,
+		},
+		ModelName:      "models/text-bison-001",
+		Temperatur:     0.7,
+		CandidateCount: 1,
+	}
+
+	for _, fn := range optFns {
+		fn(&opts)
+	}
+
+	if opts.Tokenizer == nil {
+		var tErr error
+
+		opts.Tokenizer, tErr = tokenizer.NewGPT2()
+		if tErr != nil {
+			return nil, tErr
+		}
+	}
+
+	return &Palm{
+		client: client,
+		opts:   opts,
+	}, nil
+}
+
+// Generate generates text based on the provided prompt and options.
+func (l *Palm) Generate(ctx context.Context, prompt string, optFns ...func(o *schema.GenerateOptions)) (*schema.ModelResult, error) {
+	opts := schema.GenerateOptions{
+		CallbackManger: &callback.NoopManager{},
+	}
+
+	for _, fn := range optFns {
+		fn(&opts)
+	}
+
+	res, err := l.client.GenerateMessage(ctx, &generativelanguagepb.GenerateMessageRequest{
+		Prompt:         &generativelanguagepb.MessagePrompt{},
+		Model:          l.opts.ModelName,
+		Temperature:    &l.opts.Temperatur,
+		TopP:           &l.opts.TopP,
+		TopK:           &l.opts.TopK,
+		CandidateCount: &l.opts.CandidateCount,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	generations := util.Map(res.GetCandidates(), func(m *generativelanguagepb.Message, _ int) schema.Generation {
+		switch m.GetAuthor() {
+		case "ai":
+			return schema.Generation{
+				Message: schema.NewAIChatMessage(m.GetContent()),
+				Text:    m.GetContent(),
+			}
+		case "human":
+			return schema.Generation{
+				Message: schema.NewHumanChatMessage(m.GetContent()),
+				Text:    m.GetContent(),
+			}
+		default:
+			return schema.Generation{
+				Message: schema.NewGenericChatMessage(m.GetContent(), m.GetAuthor()),
+				Text:    m.GetContent(),
+			}
+		}
+	})
+
+	return &schema.ModelResult{
+		Generations: generations,
+		LLMOutput:   map[string]any{},
+	}, nil
+}
+
+// Type returns the type of the model.
+func (l *Palm) Type() string {
+	return "llm.Palm"
+}
+
+// Verbose returns the verbosity setting of the model.
+func (l *Palm) Verbose() bool {
+	return l.opts.Verbose
+}
+
+// Callbacks returns the registered callbacks of the model.
+func (l *Palm) Callbacks() []schema.Callback {
+	return l.opts.Callbacks
+}
+
+// InvocationParams returns the parameters used in the model invocation.
+func (l *Palm) InvocationParams() map[string]any {
+	return util.StructToMap(l.opts)
+}
---
+++ model/chatmodel/palm_test.go
@@ -0,0 +1,97 @@
+package chatmodel
+
+import (
+	"context"
+	"testing"
+
+	generativelanguagepb "cloud.google.com/go/ai/generativelanguage/apiv1beta2/generativelanguagepb"
+	"github.com/googleapis/gax-go/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPalm(t *testing.T) {
+	// Create a mock PalmClient
+	mockClient := &mockPalmClient{}
+
+	// Create a Palm instance with the mock client
+	palm, err := NewPalm(mockClient)
+	assert.NoError(t, err)
+
+	// Run the test case
+	t.Run("SuccessfulGeneration", func(t *testing.T) {
+		mockClient.GenerateResponse = &generativelanguagepb.GenerateMessageResponse{
+			Candidates: []*generativelanguagepb.Message{{
+				Author:  "ai",
+				Content: "World",
+			}},
+		}
+
+		// Invoke the Generate method
+		result, err := palm.Generate(context.Background(), "Hello")
+
+		// Assert the result and error
+		assert.NoError(t, err)
+		assert.Equal(t, "World", result.Generations[0].Message.Content())
+	})
+
+	t.Run("Type", func(t *testing.T) {
+		// Create a Palm instance
+		llm, err := NewPalm(&mockPalmClient{})
+		assert.NoError(t, err)
+
+		// Call the Type method
+		typ := llm.Type()
+
+		// Assert the result
+		assert.Equal(t, "llm.Palm", typ)
+	})
+
+	t.Run("Verbose", func(t *testing.T) {
+		// Create a Palm instance
+		llm, err := NewPalm(&mockPalmClient{})
+		assert.NoError(t, err)
+
+		// Call the Verbose method
+		verbose := llm.Verbose()
+
+		// Assert the result
+		assert.False(t, verbose)
+	})
+
+	t.Run("Callbacks", func(t *testing.T) {
+		// Create a Palm instance
+		llm, err := NewPalm(&mockPalmClient{})
+		assert.NoError(t, err)
+
+		// Call the Callbacks method
+		callbacks := llm.Callbacks()
+
+		// Assert the result
+		assert.Empty(t, callbacks)
+	})
+
+	t.Run("InvocationParams", func(t *testing.T) {
+		// Create a Palm instance
+		llm, err := NewPalm(&mockPalmClient{}, func(o *PalmOptions) {
+			o.Temperatur = 0.7
+		})
+		assert.NoError(t, err)
+
+		// Call the InvocationParams method
+		params := llm.InvocationParams()
+
+		// Assert the result
+		assert.Equal(t, float32(0.7), params["temperatur"])
+	})
+}
+
+// mockPalmClient is a mock implementation of the PalmClient interface for testing.
+type mockPalmClient struct {
+	GenerateResponse *generativelanguagepb.GenerateMessageResponse
+	GenerateError    error
+}
+
+// GenerateMessage is a mock implementation of the GenerateMessage method.
+func (m *mockPalmClient) GenerateMessage(ctx context.Context, req *generativelanguagepb.GenerateMessageRequest, opts ...gax.CallOption) (*generativelanguagepb.GenerateMessageResponse, error) {
+	return m.GenerateResponse, m.GenerateError
+}
