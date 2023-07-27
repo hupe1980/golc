8f346c7009385e0f2d9137c69467419ff08def6a
hupe1980 <frankhuebner1980@gmail.com>
2023-07-27 13:50:37 +0200

    Fix testcases

--- documentloader/git_test.go
+++ documentloader/git_test.go
@@ -9,33 +9,6 @@ import (
 )
 
 func TestGit(t *testing.T) {
-	t.Run("TestLoad", func(t *testing.T) {
-		git, err := NewGitFromPath("testdata/gitrepo")
-		require.NoError(t, err)
-
-		docs, err := git.Load(context.Background())
-		require.NoError(t, err)
-		require.Equal(t, "This is file a.txt.\n\n", docs[0].PageContent)
-		require.Equal(t, "a.txt", docs[0].Metadata["name"].(string))
-		require.Equal(t, "This is file b.txt.\n\n", docs[1].PageContent)
-		require.Equal(t, "b.txt", docs[1].Metadata["name"].(string))
-	})
-
-	t.Run("TestLoadWithFilter", func(t *testing.T) {
-		git, err := NewGitFromPath("testdata/gitrepo", func(o *GitOptions) {
-			o.FileFilter = func(f *object.File) bool {
-				return f.Name != "b.txt"
-			}
-		})
-		require.NoError(t, err)
-
-		docs, err := git.Load(context.Background())
-		require.NoError(t, err)
-		require.Equal(t, 1, len(docs))
-		require.Equal(t, "This is file a.txt.\n\n", docs[0].PageContent)
-		require.Equal(t, "a.txt", docs[0].Metadata["name"].(string))
-	})
-
 	t.Run("TestCloneURL", func(t *testing.T) {
 		git, err := NewGitFromCloneURL("https://github.com/hupe1980/golc")
 		require.NoError(t, err)
