5434ed2d341aa210787ab60df4b7d84fcd8f5992
hupe1980 <frankhuebner1980@gmail.com>
2023-07-29 17:25:41 +0200

    Add security warning to go docs

--- chain/api.go
+++ chain/api.go
@@ -62,6 +62,21 @@ type APIOptions struct {
 	VerifyURL VerifyURL
 }
 
+// API represents a chain that makes API calls based on given API documentation and user questions.
+//
+// WARNING: The API chain has the potential to be susceptible to Server-Side Request Forgery (SSRF) attacks
+// if not used carefully and securely. SSRF allows an attacker to manipulate the server into making unintended
+// and unauthorized requests to internal or external resources, which can lead to potential security breaches
+// and unauthorized access to sensitive information.
+//
+// To mitigate the risks associated with SSRF attacks, it is strongly advised to use the VerifyURL hook diligently.
+// The VerifyURL hook should be implemented to validate and ensure that the generated URLs are restricted to authorized
+// and safe resources only. By doing so, unauthorized access to sensitive resources can be prevented, and the application's
+// security can be significantly enhanced.
+//
+// It is the responsibility of developers and administrators to ensure the secure usage of the API chain. We strongly recommend
+// thorough testing, security reviews, and adherence to secure coding practices to protect against potential security threats,
+// including SSRF and other vulnerabilities.
 type API struct {
 	apiRequestChain *LLM
 	apiAnswerChain  *LLM
@@ -69,6 +84,7 @@ type API struct {
 	opts            APIOptions
 }
 
+// NewAPI creates a new instance of API with the given model, API documentation, and optional functions to set options.
 func NewAPI(llm schema.Model, apiDoc string, optFns ...func(o *APIOptions)) (*API, error) {
 	opts := APIOptions{
 		InputKey:   "question",
--- chain/sql.go
+++ chain/sql.go
@@ -71,6 +71,25 @@ type SQLOptions struct {
 // SQL is a chain implementation that prompts the user to provide an SQL query
 // to run on a database. It then verifies and executes the provided SQL query
 // and returns the result.
+//
+// WARNING: The SQL chain is a powerful tool for executing SQL queries dynamically. However, it should be used with caution
+// to prevent potential SQL injection vulnerabilities. SQL injection is a serious security risk that can lead to unauthorized
+// access, data manipulation, and potentially compromising the entire database.
+//
+// To mitigate the risks of SQL injection, it is crucial to follow these best practices while using the SQL chain:
+//
+//   - Least Privilege Principle: Ensure that the database user used in the application has the least privilege necessary
+//     to perform its required tasks. Restrict the user's permissions to only the required tables and operations.
+//
+//   - Table Whitelisting or Blacklisting: Use the Tables or Exclude options to reduce the allowed tables that can be accessed
+//     via the SQL chain. This will limit the potential impact of any SQL injection attack by restricting the scope of accessible tables.
+//
+//   - VerifySQL Hook: Implement the VerifySQL hook diligently to validate and sanitize user input. This hook should be used to check
+//     and ensure that the generated SQL queries are safe and adhere to the allowed tables and queries.
+//
+// It is the responsibility of the application developers and administrators to ensure the secure usage of the SQL chain. Failure
+// to do so can lead to severe security breaches and compromise the integrity of the application and database. We strongly recommend
+// thorough testing, security reviews, and adherence to secure coding practices to protect against SQL injection and other security threats.
 type SQL struct {
 	sqldb    *sqldb.SQLDB
 	llmChain *LLM
