4bd201b3e1c25726e3484a6494b3d9073876b1c3
hupe1980 <frankhuebner1980@gmail.com>
2023-07-30 20:29:40 +0200

    Remove unused errors

--- agent/errors.go
+++ agent/errors.go
@@ -3,10 +3,8 @@ package agent
 import "errors"
 
 var (
-	ErrUnknownAgentType       = errors.New("unknown agent type")
 	ErrAgentNoReturn          = errors.New("no actions or finish was returned by the agent")
 	ErrNotFinished            = errors.New("agent not finished before max iterations")
-	ErrInputNotString         = errors.New("input is not a string")
 	ErrInvalidChainReturnType = errors.New("agent chain did not return a string")
 	ErrUnableToParseOutput    = errors.New("unable to parse agent output")
 )
