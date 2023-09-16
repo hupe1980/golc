64752982eaa3ec15b69970ad2e322d25dc3c3772
hupe1980 <frankhuebner1980@gmail.com>
2023-09-16 10:55:51 +0200

    Add missing options

--- embedding/hugging_face_hub.go
+++ embedding/hugging_face_hub.go
@@ -11,7 +11,10 @@ import (
 // Compile time check to ensure HuggingFaceHub satisfies the Embedder interface.
 var _ schema.Embedder = (*HuggingFaceHub)(nil)
 
+// HuggingFaceHubClient represents a client for interacting with Hugging Face Hub.
 type HuggingFaceHubClient interface {
+	// FeatureExtractionWithAutomaticReduction performs feature extraction with automatic reduction.
+	// It returns the extraction response or an error if the operation fails.
 	FeatureExtractionWithAutomaticReduction(ctx context.Context, req *huggingface.FeatureExtractionRequest) (huggingface.FeatureExtractionWithAutomaticReductionResponse, error)
 }
 
@@ -22,20 +25,24 @@ type HuggingFaceHubOptions struct {
 	Options huggingface.Options
 }
 
+// HuggingFaceHub represents an embedder for Hugging Face Hub models.
 type HuggingFaceHub struct {
 	client HuggingFaceHubClient
 	opts   HuggingFaceHubOptions
 }
 
+// NewHuggingFaceHub creates a new instance of the HuggingFaceHub embedder.
 func NewHuggingFaceHub(token string, optFns ...func(o *HuggingFaceHubOptions)) *HuggingFaceHub {
 	client := huggingface.NewInferenceClient(token)
 
 	return NewHuggingFaceHubFromClient(client, optFns...)
 }
 
+// NewHuggingFaceHubFromClient creates a new instance of the HuggingFaceHub embedder from a custom client.
 func NewHuggingFaceHubFromClient(client HuggingFaceHubClient, optFns ...func(o *HuggingFaceHubOptions)) *HuggingFaceHub {
 	opts := HuggingFaceHubOptions{
-		Model: "sentence-transformers/all-mpnet-base-v2",
+		Model:   "sentence-transformers/all-mpnet-base-v2",
+		Options: huggingface.Options{},
 	}
 
 	for _, fn := range optFns {
@@ -51,8 +58,9 @@ func NewHuggingFaceHubFromClient(client HuggingFaceHubClient, optFns ...func(o *
 // EmbedDocuments embeds a list of documents and returns their embeddings.
 func (e *HuggingFaceHub) EmbedDocuments(ctx context.Context, texts []string) ([][]float64, error) {
 	res, err := e.client.FeatureExtractionWithAutomaticReduction(ctx, &huggingface.FeatureExtractionRequest{
-		Inputs: texts,
-		Model:  e.opts.Model,
+		Inputs:  texts,
+		Model:   e.opts.Model,
+		Options: e.opts.Options,
 	})
 	if err != nil {
 		return nil, err
@@ -64,8 +72,9 @@ func (e *HuggingFaceHub) EmbedDocuments(ctx context.Context, texts []string) ([]
 // EmbedQuery embeds a single query and returns its embedding.
 func (e *HuggingFaceHub) EmbedQuery(ctx context.Context, text string) ([]float64, error) {
 	res, err := e.client.FeatureExtractionWithAutomaticReduction(ctx, &huggingface.FeatureExtractionRequest{
-		Inputs: []string{text},
-		Model:  e.opts.Model,
+		Inputs:  []string{text},
+		Model:   e.opts.Model,
+		Options: e.opts.Options,
 	})
 	if err != nil {
 		return nil, err
